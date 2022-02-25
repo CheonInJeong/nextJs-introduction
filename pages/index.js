2ad399078d6681ece61f486aa9b5392949616c0a
첸첸 <chenchen@chenchen-ui-MacBookPro.local>
2022-02-26 00:02:34 +0900

    API_KEY 숨기기/ redirect와 rewrite의 차이

--- pages/index.js
+++ pages/index.js
@@ -2,13 +2,12 @@
 import { useEffect, useState } from "react";
 import Seo from "../components/Seo";
 
-const API_KEY = "62336aa1be29732cf214aac930c041d1";
 export default function Home(){
     const [movies, setMovies] = useState();
 
     useEffect(()=> {
         (async () => {
-            const {results} = await (await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`)).json();
+            const {results} = await (await fetch("/api/movies")).json();
             console.log(results);
             setMovies(results);
         })();
