8a585c32990f577fe146d0315db6ce0b36adb63d
첸첸 <chenchen@chenchen-ui-MacBookPro.local>
2022-02-26 00:15:16 +0900

    server side rendering

--- pages/index.js
+++ pages/index.js
@@ -2,25 +2,26 @@
 import { useEffect, useState } from "react";
 import Seo from "../components/Seo";
 
-export default function Home(){
-    const [movies, setMovies] = useState();
+export default function Home({ results }){
 
-    useEffect(()=> {
-        (async () => {
-            const {results} = await (await fetch("/api/movies")).json();
-            console.log(results);
-            setMovies(results);
-        })();
-       
-    },[])
     return (
         <div>
            <Seo title="HOME" />
-           {!movies && <h4>Loading...</h4>}
-           {movies?.map((movie) => (
+           {results?.map((movie) => (
                 <div key={movie.key}>
                     <h4>{movie.original_title}</h4>
                 </div>))}
         </div>
         );
+}
+
+//서버에서 진행 됨. only at server side 클라이언트는 볼 수 없음
+//화면에 loding하는 것을 보여주기 싫고 데이터가 유효할 때만 보여주고 싶을 때 사용.
+export async function getServerSideProps() {
+    const {results} = await (await fetch("http://localhost:3000/api/movies")).json();
+    return {
+        props : {
+            results, //props로 전달하게 됨
+        }
+    }
 }
